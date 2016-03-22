a394b2a12ec0433a6f0b716117905c614c757556
Patrick White <pfwhite9@gmail.com>
2016-03-21 23:29:19 -0400

    modified:   leinget.sh

--- leinget.sh
+++ leinget.sh
@@ -1,5 +1,2 @@
-touch lein;
-curl https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein >| lein;
-chmod +x lein;
-export $PATH=$(pwd):$PATH;
+curl https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein >| /usr/local/bin/lein;
 ./lein;
